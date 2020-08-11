acede0861cae1d5403ecc2c599f89e512b384af6
SohyeonP <hidhen9714@naver.com>
2020-08-11 15:24:23 +0900

    Last commit_1

--- src/api/problems.js
+++ src/api/problems.js
@@ -1,4 +1,4 @@
-const API_URL = process.env.NODE_ENV == 'production'
+const API_URL = process.env.NODE_ENV === 'production'
     ? 'http://codewars-env.eba-fmrv6mpd.ap-northeast-2.elasticbeanstalk.com'
     :'http://localhost:8000/';
 
